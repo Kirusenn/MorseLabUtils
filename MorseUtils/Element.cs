01fb82ffda9141e5919ee5e5eff0a00edfc53d07
Chris Nielson <cnielson3@gmail.com>
2020-02-12 16:55:10 -0700

    Created Element class. Need to rearrange code.

---
+++ MorseUtils/Element.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseUtils {
+	class Element {
+		private String symbol;
+		private double average_mass;
+		private LinkedList<KeyValuePair<double, double>> isotopes;
+
+		protected Element() {
+			try {
+				using (StreamReader reader = new StreamReader("ATOMIC_ISOTOPES.csv")) {
+					while (!reader.EndOfStream) {
+
+					}
+				}
+			} catch (Exception e) {
+				// TODO
+			}
+		}
+	}
+}
